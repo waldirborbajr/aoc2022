709ba45703efa306a37b983402eb94b2034a72ca
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-12-01 19:34:59 -0300

    day01

--- day01/target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-/Users/borba/wks/rust/aoc2022/day01/target/debug/day01: /Users/borba/wks/rust/aoc2022/day01/src/main.rs
--- day01/target/debug/deps/day01-61058578df0b09c4.d
+++
@@ -1,5 +0,0 @@
-/Users/borba/wks/rust/aoc2022/day01/target/debug/deps/day01-61058578df0b09c4.rmeta: src/main.rs
-
-/Users/borba/wks/rust/aoc2022/day01/target/debug/deps/day01-61058578df0b09c4.d: src/main.rs
-
-src/main.rs:
