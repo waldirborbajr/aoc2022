2c02db96d8dafb9d35228f31a0b9cdcb8272fe17
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-12-02 10:29:33 -0300

    update: day02

--- src/day02.rs
+++ src/day02.rs
@@ -1,6 +1,54 @@
-use std::fs;
+fn chunks(input: &str) -> &[[u8; 4]] {
+    unsafe { std::slice::from_raw_parts(input.as_ptr().cast(), input.len() / 4) }
+}
 
-pub(crate) fn day02() {
-    println!("day02");
-    let input = fs::read_to_string("inputs/day02.txt").unwrap();
+const ROCK: u8 = b'A';
+const PAPER: u8 = b'B';
+const SCISSORS: u8 = b'C';
+
+const LOSE: u8 = b'X';
+const DRAW: u8 = b'Y';
+const WIN: u8 = b'Z';
+
+pub(crate) fn run() {
+    let input = include_str!("../inputs/day02.txt");
+
+    let mut part_1 = 0u32;
+    let mut part_2 = 0u32;
+
+    for &[theirs, _, ours, _] in chunks(input) {
+        part_1 += match (theirs, ours - (b'X' - b'A')) {
+            (ROCK, ROCK) => 3,
+            (ROCK, PAPER) => 6,
+            (ROCK, SCISSORS) => 0,
+            (PAPER, ROCK) => 0,
+            (PAPER, PAPER) => 3,
+            (PAPER, SCISSORS) => 6,
+            (SCISSORS, ROCK) => 6,
+            (SCISSORS, PAPER) => 0,
+            (SCISSORS, SCISSORS) => 3,
+            _ => unreachable!(),
+        };
+
+        part_1 += (ours - (b'X' - 1)) as u32;
+
+        let required = match (theirs, ours) {
+            (ROCK, LOSE) => SCISSORS,
+            (ROCK, DRAW) => ROCK,
+            (ROCK, WIN) => PAPER,
+            (PAPER, LOSE) => ROCK,
+            (PAPER, DRAW) => PAPER,
+            (PAPER, WIN) => SCISSORS,
+            (SCISSORS, LOSE) => PAPER,
+            (SCISSORS, DRAW) => SCISSORS,
+            (SCISSORS, WIN) => ROCK,
+            _ => unreachable!(),
+        };
+
+        part_2 += (required - (ROCK - 1)) as u32;
+        part_2 += (ours - LOSE) as u32 * 3;
+    }
+
+    println!("Part 1: {part_1}");
+    println!("Part 2: {part_2}");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 mod day01;
-// mod day02;
+mod day02;
 
 fn main() {
+    println!("----------------------------------");
+    println!("Day 01");
     day01::run();
-    // day02::run();
+    println!("----------------------------------");
+    println!("Day 02");
+    day02::run();
+    println!("----------------------------------");
+
+
+    println!(
+    "{}",
+    include_bytes!("../inputs/day02.txt")
+        .split(|b| *b == b'\n')
+        .map(|l| ((l[0] - b'A') as i16, (l[2] - b'X') as i16,))
+        .map(|(a, b)| 1 + b + 3 * ((1 + b - a).rem_euclid(3)))
+        .sum::<i16>(),
+    );
 }
