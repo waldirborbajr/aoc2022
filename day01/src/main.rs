62d3e63a8256199e7bdd5f6a92f7d059df9a43de
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-12-01 22:29:57 -0300

    update: day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,9 +1,8 @@
-use std::fs;
 
 fn main() {
-    let filepath = "day01.txt";
-    let content = fs::read_to_string(filepath)
-        .expect("Cannot open file");
+    // let filepath = "day01.txt";
+    // let content = fs::read_to_string(filepath)
+    //     .expect("Cannot open file");
 
     // Part I
     // let result: u32 = content
@@ -21,25 +20,40 @@ fn main() {
         // .collect();
 
     // Part II
-    let mut result: Vec<u32> = content
-        .split("\n\n")
-        .map(|block|
-            block
-            .split("\n")
-            .filter(|item| !item.is_empty())
-            .map(|item| item.parse::<u32>().unwrap())
-            .sum::<u32>()
+    // let mut result: Vec<u32> = content
+    //     .split("\n\n")
+    //     .map(|block|
+    //         block
+    //         .split("\n")
+    //         .filter(|item| !item.is_empty())
+    //         .map(|item| item.parse::<u32>().unwrap())
+    //         .sum::<u32>()
+    //     )
+    //     .collect();
+    //
+    //     result.sort_by(|a,b| b.cmp(a));
+    //
+    // let sum: u32 = result
+    //     .iter()
+    //     .take(3)
+    //     .sum();
+
+    let data: &str = include_str!("../day01.txt").trim();
+
+    let mut elves: Vec<usize> = data.split("\n\n")
+        .map(|elf| elf.split("\n")
+            .map(|e| e.parse::<usize>().unwrap())
+            .sum()
         )
-        .collect();
+    .collect();
 
-        result.sort_by(|a,b| b.cmp(a));
+    elves.sort();
+    elves.reverse();
 
-    let sum: u32 = result
-        .iter()
-        .take(3)
-        .sum();
+    println!("Part I: {}",
+        elves[0]);
 
-    println!("{:?}", sum);
-
-
+    println!("Part II: {}",
+        elves.iter().take(3).sum::<usize>()
+    );
 }
